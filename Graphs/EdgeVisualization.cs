77f7330947c9bf72f755cdf1fe92c6a4dd060c15
Mateusz Rybarski <rybarski.mateusz@gmail.com>
2014-12-06 14:29:43 +0100

    drawing arrow

--- Graphs/EdgeVisualization.cs
+++ Graphs/EdgeVisualization.cs
@@ -30,12 +30,42 @@ namespace Graphs {
 			
 
 		private void DrawEdge(Cairo.Context cx, NodeVisualization _from, NodeVisualization _to) {
+			int beginX = _from.X + _from.Width / 2;
+			int beginY = _from.Y + _from.Height / 2;
+			int endX = _to.X + _to.Width / 2;
+			int endY = _to.Y + _to.Height / 2;
+
 			cx.Antialias = Antialias.Gray;
 			cx.LineWidth = 3;
 			cx.SetSourceColor(new Color (0, 0, 0, 1));
 			cx.LineCap = LineCap.Round;
-			cx.MoveTo (_from.X + _from.Width/2, _from.Y + _from.Height/2);
-			cx.LineTo (_to.X + _to.Width/2, _to.Y + _to.Height/2);
+			cx.MoveTo (beginX, beginY);
+			cx.LineTo (endX, endY);
+			cx.Stroke ();
+
+			int tipX = (endX + beginX) / 2;
+			int tipY = (endY + beginY) / 2;
+
+			int arrowLength = 10; //can be adjusted
+			int dx = endX - beginX;
+			int dy = endY - beginY;
+
+			double theta = Math.Atan2(dy, dx);
+
+			double rad = 35*Math.PI/180; //35 angle, can be adjusted
+			double x = tipX - arrowLength * Math.Cos(theta + rad);
+			double y = tipY - arrowLength * Math.Sin(theta + rad);
+
+			double phi2 = -35*Math.PI/180;//-35 angle, can be adjusted
+			double x2 = tipX - arrowLength * Math.Cos(theta + phi2);
+			double y2 = tipY - arrowLength * Math.Sin(theta + phi2);
+
+			cx.MoveTo (tipX, tipY);
+			cx.LineTo (x, y);
+			cx.Stroke ();
+
+			cx.MoveTo (tipX, tipY);
+			cx.LineTo (x2, y2);
 			cx.Stroke ();
 		}
 	}
